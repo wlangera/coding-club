5c238c948bad96865c58d2ed5379c8a302d09a94
wlangera <ward.langeraert@inbo.be>
2022-11-24 11:07:42 +0100

    challenge 1

--- src/20221124/CopyOf20221124_challenges_copy.R
+++ src/20221124/CopyOf20221124_challenges_copy.R
@@ -4,16 +4,22 @@ library(sf)
 library(exactextractr)
 library(geodata)
 
+data_path <- "./data/20221124/"
 
 ## CHALLENGE 1
+# 1. Read in file
+lu_nara_2016 <- rast(paste0(data_path, "20221124_lu_nara_2016_100m.tif"))
 
+# 2. Which layers (names) does it contain? What is the crs? The extent?
+# The resolution? What are the minmax values of the raster?
+lu_nara_2016
 
-
+# 3. plot
+plot(lu_nara_2016)
 
 # 4. Read `20211125_lu_nara_legend.csv` (code is provided!) so that you can set
 # the color table associated with `lu_nara_2016`.
-legend <- read_csv(
-  "./data/20211125/20211125_lu_nara_legend.csv",
+legend <- read_csv(paste0(data_path, "20221124_lu_nara_legend.txt"),
   col_types = cols(
     id = col_double(),
     land_use = col_character(),
@@ -21,13 +27,21 @@ legend <- read_csv(
   ))
 legend
 
+plot(lu_nara_2016, type = "classes", col = legend$color,
+     levels = legend$land_use)
 
+lu_nara_2016_merge <- merge(lu_nara_2016, legend)
 
-#' 5. Calculate the most and least occurring land use category in each of the
-#' Natura2000 protected areas (read these areas using the provided code). Tip: use exactextractr::exact_extract() and find
-#' the right [summary operations](https://isciences.gitlab.io/exactextractr/#summary-operations)
+# 5. Calculate the most and least occurring land use category in each of the
+# Natura2000 protected areas (read these areas using the provided code).
+# Tip: use exactextractr::exact_extract() and find the right
+# [summary operations](https://isciences.gitlab.io/exactextractr/#summary-operations)
 natura2000 <- st_read("./data/20211125/20211125_protected_areas_Lambert72.gpkg",
                       layer = "ps_hbtrl")
+plot(natura2000)
+
+min_max_lu <- exactextractr::exact_extract(lu_nara_2016, natura2000,
+                                           c("minority", "majority"))
 
 
 
