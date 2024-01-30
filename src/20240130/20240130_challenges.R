f3c14156e6edf64ce015d25926810fb565d28341
wlangera <ward.langeraert@inbo.be>
2024-01-30 10:25:55 +0100

    challenge 1

---
+++ src/20240130/20240130_challenges.R
@@ -0,0 +1,31 @@
+# Load packages
+library(tidyverse)
+
+# Read in data
+birds <- read_tsv("./data/20240130/20240130_bird_rings.txt")
+sc_names <- read_csv("./data/20240130/20240130_scientificnames.txt")
+sc_names <- sc_names$scientific_name
+
+## Challenge 1
+# Look at data
+glimpse(birds)
+
+#color_ring: column containing the color rings
+#metal_ring: column containing the metal rings
+
+#1.Get the length of the metal rings.
+sort(unique(str_length(birds$metal_ring)))
+
+#2. Do the color rings start with a "C"?
+all(str_starts(birds$color_ring, "C"))
+
+#3. Do the color rings end with a "R"?
+all(str_ends(birds$color_ring, "R"))
+
+#4. Are the color rings uppercase?
+all(str_detect(birds$color_ring, "[a-z]", negate = TRUE))
+
+#5. Solve all the anomalies found in (4) by setting all color rings uppercase.
+birds_cleaned <- birds %>%
+  mutate(color_ring = str_to_upper(color_ring))
+all(str_detect(birds_cleaned$color_ring, "[a-z]", negate = TRUE))
