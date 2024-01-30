33bd35651c6b4f42a13338fb51baf4e06fbe9fea
wlangera <ward.langeraert@inbo.be>
2024-01-30 10:30:12 +0100

    challenge 2 questions

--- src/20240130/20240130_challenges.R
+++ src/20240130/20240130_challenges.R
@@ -6,6 +6,7 @@ birds <- read_tsv("./data/20240130/20240130_bird_rings.txt")
 sc_names <- read_csv("./data/20240130/20240130_scientificnames.txt")
 sc_names <- sc_names$scientific_name
 
+
 ## Challenge 1
 # Look at data
 glimpse(birds)
@@ -29,3 +30,20 @@ all(str_detect(birds$color_ring, "[a-z]", negate = TRUE))
 birds_cleaned <- birds %>%
   mutate(color_ring = str_to_upper(color_ring))
 all(str_detect(birds_cleaned$color_ring, "[a-z]", negate = TRUE))
+
+
+## Challenge 2
+#1. Create a new column called color_ring_complete containing color ring
+# information in this format:
+# background_color+inscription_color+"("+color_ring+")", e.g. RW(FJAC)
+
+#2. Are the color rings 4-letter only long and is the third letter an "A"?
+
+#3. Do the color rings contain at least a digit?
+
+#4. Create a new column called digit containing the first digit, if any,
+# as a number.
+
+#5. By combining dplyr and stringr, select the birds whose color rings satisfy
+# the condition in (2).
+
