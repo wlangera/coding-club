4cdefeb041d8a3394f03c917273177008e517b94
wlangera <ward.langeraert@inbo.be>
2024-04-25 10:39:47 +0200

    challenge 1

--- src/20240425/CopyOf20240425_challenges.R
+++ src/20240425/CopyOf20240425_challenges.R
@@ -9,32 +9,58 @@ obs <- readr::read_csv("./data/20240425/20240425_observations.csv")
 glimpse(obs)
 
 # 1. Display first 8 rows; display the last 8 rows.
-
+slice_head(obs, n = 8) # first
+slice_tail(obs, n = 8) # last
 
 # 2. Select columns observationLevel, eventStart, eventEnd and scientificName
-
+obs %>%
+  select("observationLevel", "eventStart", "eventEnd", "scientificName")
 
 # 3. Display the distinct values of observationLevel as a data.frame with one
 # column, observationLevel. How many different values of observationLevel are
 # there?
+obs %>%
+  distinct(.data$observationLevel)
 
+# how many?
+obs %>%
+  distinct(.data$observationLevel) %>%
+  nrow()
 
 # 4. How to remove observations with observationLevel = "media"? How to remove
 # observations with observationLevel = "media" and empty scientificName?
+obs %>%
+  filter(.data$observationLevel != "media")
 
+obs %>%
+  filter(.data$observationLevel != "media",
+         !is.na(.data$scientificName))
 
 # 5. Add a new column called is_classified_by_human which is TRUE if
 # classificationMethod is equal to "human". Add a new column month with the
 # month of the observation (eventStart). Tip: use lubridate's month() and year()
 # functions.
 
-# 6. Move the column count after behavior.
+obs %>%
+  mutate(is_classified_by_human = .data$classificationMethod == "human",
+         month = month(.data$eventStart))
 
-# 7. Move all columns starting with "taxon." before observationLevel
+# 6. Move the column count after behavior.
+obs %>%
+  relocate("count", .after = "behavior")
+
+# 7. Move all columns starting with "individual." before observationLevel
+obs %>%
+  relocate(starts_with("individual"), .before = "observationLevel")
 
 # 8. Is the following statement true of false: Observations with
 # observationLevel = "event" have no mediaID, i.e. mediaID is NA.
-
+obs %>%
+  filter(.data$observationLevel == "event") %>%
+  pull("mediaID") %>%
+  # are they all NA?
+  is.na() %>%
+  all()
 
 
 ## CHALLENGE 2 - Summaries
